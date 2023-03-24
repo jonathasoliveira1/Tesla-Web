344501e15970a1a03c8343815e5602230147d6f2
Jonathas Oliveira Souza <jonathas.souza.bsntec@gmail.com>
2023-03-24 14:32:43 -0300

    Implementação do CSS & Correção do JavasCript

--- js/index.js
+++ js/index.js
@@ -1,21 +1,17 @@
-var header = document.getElementById("header");
+var header= document.getElementById("header");
 
-var ms=
-document.getElementById("models");
-var ms3=
-document.getElementById("models3");
-var msx=
-document.getElementById("modelsx");
-var msy=
-document.getElementById("modelsy");
-var model=
-document.getElementById("model");
+var ms= document.getElementById("models");
+var m3= document.getElementById("model3");
+var mx= document.getElementById("modelx");
+var my= document.getElementById("modely");
+var model= document.getElementById("model");
+
 
 ms.onclick = function() {
     header.style.backgroundImage = "url(https://i.postimg.cc/mg7L1t7Z/image-1.png)";
     model.innerHTML="Model S";
 }
-ms3.onclick = function(){
+m3.onclick = function(){
     header.style.backgroundImage="url(https://i.postimg.cc/FRCNXcXp/image-2.png)";
     model.innerHTML="Model 3";
 }
@@ -27,3 +23,20 @@ my.onclick = function() {
     header.style.backgroundImage="url(https://i.postimg.cc/FRCNXcXp/image-4.png)";
                 model.innerHTML="Model Y";
 }
+
+            ms.onclick=function(){
+                header.style.backgroundImage="url(https://i.postimg.cc/mg7L1t7Z/image-1.png)";
+                model.innerHTML="Model S";
+            }
+            m3.onclick=function(){
+                header.style.backgroundImage="url(https://i.postimg.cc/FRCNXcXp/image-2.png)";
+                model.innerHTML="Model 3";
+            }
+            mx.onclick=function(){
+                header.style.backgroundImage="url(https://i.postimg.cc/KYNmQw9d/image-3.png)";
+                model.innerHTML="Model X";
+            }
+            my.onclick=function(){
+                header.style.backgroundImage="url(https://i.postimg.cc/1t3sdB24/image-4.png)";
+                model.innerHTML="Model Y";
+            }
